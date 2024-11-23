e8d4184b57cbafe042152bd77c8417f4537f52c7
saoliaa <palijanna23@gmail.com>
2024-11-23 22:21:05 +0300

    Done lab6

--- projects/2_query/main.go
+++ projects/2_query/main.go
@@ -1,9 +1,25 @@
 package main
 
-// здесь надо написать код
+// некоторые импорты нужны для проверки
+import (
+	"fmt"
+	"net/http" // пакет для поддержки HTTP протокола
+)
 
-func main() {
-	// и здесь тоже
+func handler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "Guest"
+	}
+	response := fmt.Sprintf("Hello,%s!", name)
+
+	fmt.Fprintln(w, response)
 }
 
-// и тут тоже (если очень надо)
+func main() {
+	http.HandleFunc("/api/user", handler)
+	err := http.ListenAndServe(":9000", nil)
+	if err != nil {
+		fmt.Println("Ошибка запуска сервера:", err)
+	}
+}
